937cecd67a9da0379b7a9b4e76015624466f6717
submitCode <wilhelm.fritsche@gmail.com>
2019-06-05 21:16:52 +0200

    first running version

---
+++ src/data/voc_data_reader.py
@@ -0,0 +1,81 @@
+"""
+This module  is the amended example from
+http://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
+in order to be able to use the VOC Dataset for testing purposes
+"""
+
+import os
+import xml.etree.ElementTree as et
+import pathlib
+from PIL import Image
+import torch
+
+
+CLASS_NAMES = ['aeroplane', 'bicycle', 'bird', 'boat',
+               'bottle', 'bus', 'car', 'cat', 'chair',
+               'cow', 'diningtable', 'dog', 'horse',
+               'motorbike', 'person', 'pottedplant',
+               'sheep', 'sofa', 'train', 'tvmonitor']
+
+
+class VOCDataReader(object):
+    """
+    this class expects the images in an JPEGImages folder and an Annotations
+    folder.
+    """
+    def __init__(self, root, transforms=None, object_categories=CLASS_NAMES):
+        self.root = pathlib.Path(root)
+        self.path_images = self.root / 'JPEGImages'
+        self.path_annotations = self.root / 'Annotations'
+        self.transforms = transforms
+        self.class_names = object_categories
+        self.num_classes = len(object_categories)
+
+        assert self.path_images.exists(), "Folder JPEGImages is expected but does not exist"
+        assert self.path_annotations.exists(), "Folder Annotations is expected but does not exist"
+        self.imgs = list(sorted(os.listdir(str(self.path_images))))
+        self.annotations = list(sorted(os.listdir(str(self.path_annotations))))
+
+    def __getitem__(self, idx):
+        # load images ad masks
+        img_path = self.path_images / self.imgs[idx]
+        annotation_path = self.path_annotations / self.annotations[idx]
+        img = Image.open(img_path).convert("RGB")
+        annotation_root = et.parse(annotation_path).getroot()
+
+
+        # get bounding box coordinates for each mask
+        boxes = []
+        xml_boxes = list(annotation_root.findall('object'))
+        num_objs = len(xml_boxes)
+        labels = []
+        for i, element in enumerate(xml_boxes):
+            box = list(element.find('bndbox'))
+            xmin = int(box[0].text)
+            ymin = int(box[1].text)
+            xmax = int(box[2].text)
+            ymax = int(box[3].text)
+            boxes.append([xmin, ymin, xmax, ymax])
+
+            label = element.find('name').text
+            labels.append(CLASS_NAMES.index(label))
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        image_id = torch.as_tensor([idx])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
---
+++ test/test_vocdatareader.py
@@ -0,0 +1,51 @@
+""" Tests for the VOCDataReader class """
+
+import unittest
+import pathlib
+import sys
+from src.data.voc_data_reader import VOCDataReader
+
+
+class TestVOCDataReader(unittest.TestCase):
+    """ Test class """
+
+    def setUp(self):
+        """ set object_categories and test data path """
+        self.path_root = pathlib.Path('test/test_data')
+        self.object_categories = ['aeroplane', 'bicycle', 'bird', 'boat',
+                                  'bottle', 'bus', 'car', 'cat', 'chair',
+                                  'cow', 'diningtable', 'dog', 'horse',
+                                  'motorbike', 'person', 'pottedplant',
+                                  'sheep', 'sofa', 'train', 'tvmonitor']
+
+    def test_constructor(self):
+        """ test the object initialization """
+        data_reader = VOCDataReader(
+            str(self.path_root),
+            object_categories=self.object_categories)
+        self.assertEqual(len(data_reader.imgs), 3)
+        self.assertEqual(len(data_reader.annotations), 3)
+
+    def test_getitem(self):
+        """ test the __getitem__ function """
+        data_reader = VOCDataReader(
+            str(self.path_root),
+            object_categories=self.object_categories
+        )
+
+        img, target = data_reader.__getitem__(0)
+        self.assertEqual(len(target['boxes']), 5)
+        self.assertEqual(target['boxes'][0][0].numpy(), 263)
+        self.assertEqual(target['boxes'][0][1].numpy(), 211)
+        self.assertEqual(target['boxes'][0][2].numpy(), 324)
+        self.assertEqual(target['boxes'][0][3].numpy(), 339)
+        self.assertEqual(img.size, (500, 375))
+        self.assertEqual(target['labels'][0].numpy(), 8)
+
+    def test__len__(self):
+        """ test len function """
+        data_reader = VOCDataReader(
+            str(self.path_root),
+            object_categories=self.object_categories
+        )
+        self.assertEqual(data_reader.__len__(), 3)
